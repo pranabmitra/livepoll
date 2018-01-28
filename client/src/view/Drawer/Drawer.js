c8f54633a40bf3e4a2ddab18d8f6c70a149b3e61
minhaz.raufoon <minhaz.raufoon@ipvisionsoft.com>
2018-01-28 15:07:59 +0600

    app shell: on progress

---
+++ client/src/view/Drawer/Drawer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Drawer = () => (
+    <div className='drawer drawer-resp'>
+
+    </div>
+)
+
+export default Drawer;
---
+++ client/src/view/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Header.css'
+
+const Header = () => (
+    <div className='header header-resp fl'>
+        <button className='drawer-btn drawer-btn-resp fr'>V</button>
+        <a className='app-title app-title-resp fl'>livepoll</a>
+    </div>
+)
+
+export default Header;
