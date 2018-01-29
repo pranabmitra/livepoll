26a7a0c57763edac92b3cbf66107366e9a15946f
minhaz.raufoon <minhaz.raufoon@ipvisionsoft.com>
2018-01-29 16:05:05 +0600

    app shell: fake content

--- client/src/view/Content/Content.js
+++ client/src/view/Content/Content.js
@@ -4,7 +4,16 @@ import './Content.css';
 
 const Content = () => (
     <div className='content content-resp'>
-
+        {
+            (()=>{
+                var fake =[];
+                for (var i=0; i<100; i++) {
+                    fake.push(<span key={i + '1'}>Content</span>);
+                    fake.push(<br key={i + '2'}/>);
+                }
+                return fake;
+            })()
+        }
     </div>
 )
 
