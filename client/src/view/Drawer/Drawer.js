135bf5e5d936529ecdaa4f24b19ce0ab1f8bf86f
minhaz.raufoon <minhaz.raufoon@ipvisionsoft.com>
2018-01-29 17:55:23 +0600

    app shell: drawer auth badge fake

--- client/src/view/Drawer/Drawer.js
+++ client/src/view/Drawer/Drawer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import './Drawer.css';
+import DrawerAuthBadge from './DrawerAuthBadge/DrawerAuthBadge';
 
 const Drawer = () => (
     <div id='app-drawer' className='drawer drawer-resp'>
+        <div className='drawer-content'>
+            <DrawerAuthBadge/>
+        </div>
     </div>
 )
 
---
+++ client/src/view/Drawer/DrawerAuthBadge/DrawerAuthBadge.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './DrawerAuthBadge.css';
+
+const DrawerAuthBadge = () => (
+    <div className='drawer-auth-badge'>
+        <div className='drawer-auth-badge-img drawer-auth-badge-img-resp'></div>
+    </div>
+)
+
+export default DrawerAuthBadge;
