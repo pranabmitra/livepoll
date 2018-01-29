870c69a7f4853876e694e66c09a7e77f799d77ec
Raufoon <minhazraufooncsedu@gmail.com>
2018-01-30 01:35:59 +0600

    drawer auth badge

--- client/src/view/Drawer/DrawerAuthBadge/DrawerAuthBadge.js
+++ client/src/view/Drawer/DrawerAuthBadge/DrawerAuthBadge.js
@@ -5,6 +5,7 @@ import './DrawerAuthBadge.css';
 const DrawerAuthBadge = () => (
     <div className='drawer-auth-badge'>
         <div className='drawer-auth-badge-img drawer-auth-badge-img-resp'></div>
+        <a href='/profile' className='drawer-auth-badge-txt drawer-auth-badge-txt-resp'>Snoopy</a>
     </div>
 )
 
