395d1a05ffab93994879d5d27488d70cab77bd52
minhaz.raufoon <minhaz.raufoon@ipvisionsoft.com>
2018-01-31 11:24:59 +0600

    LPButton refactored

--- client/src/view/view-factories/button-factory.js
+++ client/src/view/view-factories/button-factory.js
@@ -5,11 +5,11 @@ export const LPButton = (props) => {
     var { btnClass, btnIconClass, iconUrl, children, btnTextClass, onClick } = props;
 
     return (
-        <button className={`lp-btn lp-btn-resp ${btnClass || ''}`} onClick={onClick}>
+        <a className={`lp-btn lp-btn-resp ${btnClass || ''}`} onClick={onClick}>
             {
                 !!iconUrl && <div className={`lp-btn-img lp-btn-img-resp ${btnIconClass || ''}`} style={{backgroundImage: `url(${iconUrl})`}}></div>
             }
             <div className={`lp-btn-txt lp-btn-txt-resp ${btnTextClass || ''}`}>{children}</div>
-        </button>
+        </a>
     )
 }
