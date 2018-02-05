a4d328576fd53104a915f2d781ac01dd326492f2
Raufoon <minhazraufooncsedu@gmail.com>
2018-02-05 20:54:53 +0600

    update

--- client/src/view/shared-views/popups/Modal.js
+++ client/src/view/shared-views/popups/Modal.js
@@ -6,41 +6,31 @@ const modalRoot = document.getElementById('modal-root');
 class Modal extends React.Component {
     constructor(props) {
         super(props);
-        this.child1 = document.createElement('div');
-        this.child1.classList.add('modal-vc');
-
-        this.child2 = document.createElement('div');
-        this.child2.classList.add('modal-hc');
-
-        this.child1.appendChild(this.child2);
-
-        if (this.props.className) this.child2.classList.add(this.props.className);
+        this.modalElem = document.createElement('div');
+        this.modalElem.classList.add('modal-vc');
 
         this.close = this.close.bind(this);
     }
 
-    close() {
-        ReactDOM.unmountComponentAtNode(this.child1);
-        modalRoot.removeChild(this.child1);
-    }
-
     componentDidMount(){
-        modalRoot.appendChild(this.child1);
+        modalRoot.appendChild(this.modalElem);
     }
 
     componentWillUnmount() {
-        modalRoot.removeChild(this.child1);
+        modalRoot.removeChild(this.modalElem);
     }
 
     render() {
         return ReactDOM.createPortal(
             (
-                <div className='modal-final-wrap'>
-                    <button className='modal-close-btn' onClick={this.close}>X</button>
-                    {this.props.children}
+                <div className='modal-hc'>
+                    <div className='modal-final-wrap'>
+                        <button className='modal-close-btn'>X</button>
+                        {this.props.children}
+                    </div>
                 </div>
             ),
-            this.child2,
+            this.modalElem,
         );
     }
 }
