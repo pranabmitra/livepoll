d4eb6c917bf98a58617e40d378b26ffa59865621
Raufoon <minhazraufooncsedu@gmail.com>
2018-02-16 01:12:42 +0600

    poll creation: working on

--- client/src/view/shared-views/CreatePollForm/CreatePollForm.js
+++ client/src/view/shared-views/CreatePollForm/CreatePollForm.js
@@ -5,9 +5,42 @@ import {fromJS} from 'immutable'
 import './CreatePollForm.css';
 import {defaultPollSettings} from "../../../control/livepoll/default-poll-settings";
 
+const inputErrorSpanStyle = {
+    color: 'red',
+    fontSize: 'small'
+}
+const EnhancedFormFieldStyle = {
+    border: '1px solid lightgray',
+    borderRadius: '5px'
+}
+const EnhancedFormField = props => (
+    <div style={EnhancedFormFieldStyle}>
+        {props.title && <label>{props.title}</label>}
+        <div>
+            {props.children}
+        </div>
+        {
+            (props.meta.touched || props.meta.dirty)
+            && props.meta.error
+            && <span style={inputErrorSpanStyle}>{props.meta.error}</span>
+        }
+    </div>
+)
+
+const PollTitleInput = props => {
+    const {input: {value, onChange}, meta} = props;
+    const onChangeTitle = event => onChange(event.target.value);
+    return (
+        <EnhancedFormField meta={meta}>
+            <input type='text' className='no-border w100p' onChange={onChangeTitle} placeholder='Give a nice title'/>
+        </EnhancedFormField>
+    )
+}
+
 const CreatePollForm = (props) => {
     return (
-        <form onSubmit={()=>{}}>
+        <form>
+            <Field name='title' component={PollTitleInput}/>
         </form>
     )
 }
