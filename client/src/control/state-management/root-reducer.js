6dba33a548ad1279e9efff291d921c0f6aeedb0d
Raufoon <minhazraufooncsedu@gmail.com>
2018-02-15 21:22:36 +0600

    redux form

--- client/src/control/state-management/root-reducer.js
+++ client/src/control/state-management/root-reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux-immutable';
 import {routerReducer} from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form/immutable'
 
 import authReducer from "./reducers/auth-reducers/auth-reducer";
 import viewReducer from "./reducers/view-reducers/view-reducer";
@@ -8,6 +9,7 @@ const reducerMap = {
     authState: authReducer,
     viewState: viewReducer,
     routeState: routerReducer,
+    form: formReducer
 };
 
 const rootReducer = combineReducers(reducerMap)
