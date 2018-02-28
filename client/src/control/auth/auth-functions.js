b07b244db049a0fdc2da870a9dcb4eef7d3db45d
minhaz.raufoon <minhaz.raufoon@ipvisionsoft.com>
2018-02-28 14:48:40 +0600

    fix

--- client/src/control/auth/auth-functions.js
+++ client/src/control/auth/auth-functions.js
@@ -1,8 +1,6 @@
 import * as firebase from 'firebase';
 
 import {SIGNIN_METHODS} from "../../constants/auth";
-import {FLOATING_MSG_TYPES} from "../../constants/popups";
-import showFloatingMsg from "../../view/shared-views/popups/floating-msg";
 
 
 export const signIn = (signInMethod) => {
@@ -15,33 +13,16 @@ export const signIn = (signInMethod) => {
 
         case SIGNIN_METHODS.FACEBOOK:
             provider = new firebase.auth.FacebookAuthProvider();
-            // TODO:: need to create FB app and set appSecret in firebase
-            // showFloatingMsg('Feature coming soon!', FLOATING_MSG_TYPES.INFO)
             break;
 
         default:
             break;
     }
 
-    // let signInFunction = isMobileDevice() ? 'signInWithRedirect' : 'signInWithPopup';
-
     try {
-        return firebase.auth().signInWithPopup(provider)
-            .then(function onSuccess(result) {
-                // const token = result.credential.accessToken;
-                // const user = result.user;
-
-                showFloatingMsg('Signed in successful!', FLOATING_MSG_TYPES.SUCCESS, 1000)
-            })
-            .catch(function onError(error) {
-                var errorCode = error.code,
-                    errorMessage = error.message,
-                    credential = error.credential;
-
-                return showFloatingMsg(errorCode + ': Sign in failed for ' + credential + ' - ' + errorMessage, FLOATING_MSG_TYPES.ERROR);
-            });
+        return firebase.auth().signInWithPopup(provider);
     } catch (error) {
-        return showFloatingMsg('Sign in failed!', FLOATING_MSG_TYPES.ERROR);
+        return Promise.reject(404);
     }
 }
 
